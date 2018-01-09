16bb5477eb7bc33953c3d44777d1ec18817a984d
JackCme <pitou_106@naver.com>
2018-01-09 19:25:47 +0900

    update Current limit on start

--- routes/index.js
+++ routes/index.js
@@ -7,15 +7,88 @@ var querystring = require('querystring');
 var https = require('https');
 var request = require('request');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-	var wsLivePath = wsconfig.host + '/live?' + querystring.stringify({sdids : wsconfig.devices.device1.id, authorization : 'bearer+'+wsconfig.devices.device1.token, uid : wsconfig.uid });
+var wsLivePath = wsconfig.host + '/live?' + querystring.stringify({sdids : wsconfig.devices.device1.id, authorization : 'bearer+'+wsconfig.devices.device1.token, uid : wsconfig.uid });
 	var wsLivePath2 = wsconfig.host + '/live?' + querystring.stringify({sdids : wsconfig.devices.device2.id, authorization : 'bearer+'+wsconfig.devices.device2.token, uid : wsconfig.uid });
 	var wsLivePath3 = wsconfig.host + '/live?' + querystring.stringify({sdids : wsconfig.devices.device3.id, authorization : 'bearer+'+wsconfig.devices.device3.token, uid : wsconfig.uid });
+var ws = new WebSocket(querystring.unescape(wsLivePath));
+var ws2 = new WebSocket(querystring.unescape(wsLivePath2));
+var ws3 = new WebSocket(querystring.unescape(wsLivePath3));
+
+var boilerRuleIdArray = ruleconfig.boilerState.lessThan;
+var humidRuleIdArray = ruleconfig.humidifierState.lessThan;
+
+var boilerRuleCount = 0;
+var humidRuleCount = 0;
+
+var tempLimits = [];
+var humidLimits = [];
+
+function getCurrentTempLimit(arr, socket) {
+	console.log("CurrentTempLimit");
+	if(boilerRuleCount >= 3) {
+		socket.emit('tempLimit', arr);
+		return;
+	}
+
+	var path = '/v1.1/rules/';
+	var options = {
+		baseUrl : 'https://api.artik.cloud',
+		method: 'GET',
+		headers : {
+			"Content-Type": "application/json",
+			"Authorization": "Bearer " + wsconfig.userToken
+		}
+	}
+		
+	options.uri = path + boilerRuleIdArray[boilerRuleCount].id;
+	console.log(options);
+	request(options, function(error, response, data) {
+		console.log(data);
+		if(!error && response.statusCode === 200) {
+			if(boilerRuleCount < 4) {
+				boilerRuleCount++;
+				console.log("boilerRuleCount: %d" + boilerRuleCount);
+				tempLimits.push(JSON.parse(data).data.rule.if.and[0].operand.value);
+				getCurrentTempLimit(arr, socket);
+			}
+		}
+	});
+}
+
+function getCurrentHumidLimit(arr, socket) {
+	if(humidRuleCount >= 3) {
+		socket.emit('humidLimit', arr);
+		return ;
+	}
+	var path = '/v1.1/rules/';
+	var options = {
+		baseUrl : 'https://api.artik.cloud',
+		method: 'GET',
+		headers : {
+			"Content-Type": "application/json",
+			"Authorization": "Bearer " + wsconfig.userToken
+		}
+	}
+	options.uri = path + humidRuleIdArray[humidRuleCount].id;
+	request(options, function(error, response, data) {
+		if(!error && response.statusCode === 200) {
+			humidRuleCount++;
+			arr.push(JSON.parse(data).data.rule.if.and[0].operand.value);
+			getCurrentHumidLimit(arr, socket);
+		}
+	});
+	
+}
+
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+	boilerRuleCount = 0;
+	humidRuleCount = 0;
+	tempLimits.length = 0;
+	humidLimits.length = 0;
+
 	req.io.on('connect', function(socket) {
-		var ws = new WebSocket(querystring.unescape(wsLivePath));
-		var ws2 = new WebSocket(querystring.unescape(wsLivePath2));
-		var ws3 = new WebSocket(querystring.unescape(wsLivePath3));
 		ws.on('message', function(data) {
 			var message = JSON.parse(data);
 			if(message.data) {
@@ -37,42 +110,15 @@ router.get('/', function(req, res, next) {
 			}
 
 		});
+		socket.on('getLimitData', function() {
+			getCurrentTempLimit(tempLimits, socket);
+	//		getCurrentHumidLimit(humidLimits, socket);
+		});
+
+		
 	})
 
-	var path = '/v1.1./rules/';
-	var boilerRuleIdArray = ruleconfig.boilerState.lessThan;
-	var humidRuleIdArray = ruleconfig.humidifierState.lessThan;
-	var tempLimits = [];
-	var humidLimits = [];
-	var options = {
-		baseUrl : 'https://api.artik.cloud',
-		method: 'GET',
-		headers : {
-			"Content-Type": "application/json",
-			"Authorization": "Bearer " + wsconfig.userToken
-		}
-	}
-	boilerRuleIdArray.forEach((value) => {
-		options.uri = path + value.id;
-		console.log(options);
-		request(options, function(err, res, data) {
-			console.log(data);
-			if(!err && res.statusCode === 200) {
-				tempLimits.push(JSON.parse(data).data.rule.if.and[0].operand.value);
-			}
-			console.log(tempLimits);
-		});
-	});
 
-	humidRuleIdArray.forEach((value) => {
-		options.uri = path + value.id;
-		request(options, function(err, res, data) {
-			if(!err && res.statusCode === 200) {
-				humidLimits.push(JSON.parse(data).data.rule.if.and[0].operand.value);
-			}
-			console.log(humidLimits);
-		});
-	});
 
   res.render('index', { title: 'Express' });
 	
